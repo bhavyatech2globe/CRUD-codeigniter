ac2d4f3939773dd58bf52985bfc5382803116cdc
bhavyatech2globe <bhavya.khanna@tech2globe.in>
2023-05-30 17:35:29 +0530

    CRD part of CRUD

---
+++ app/Views/templates/header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <title>CodeIgniter Tutorial</title>
+</head>
+<body>
+
+<nav>
+    <a href="/news">view</a>
+    <a href="/news/create">create</a>
+</nav>
+
+    <h1><?= esc($title) ?></h1>
